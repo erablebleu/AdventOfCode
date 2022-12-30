d7a5f1020643ae6aea2559c6ab1de12928d7b4a8
MR <moayeut@gmail.com>
2022-12-30 09:56:47 +0100

    2016_04 - some years later

---
+++ 2016/2016_04/2016_04.cs
@@ -0,0 +1,35 @@
+namespace AdventOfCode;
+
+/// <summary>
+/// https://adventofcode.com/2016/day/04
+/// </summary>
+public class _2016_04 : Problem
+{
+    private (string Encrypted, int SectorId, string Checksum)[] _data;
+
+    public override void Parse()
+    {
+        _data = Inputs.Select(l => Parse(l)).ToArray();
+    }
+
+    public override object PartOne() => _data.Where(r => GetChecksum(r.Encrypted) == r.Checksum).Sum(r => r.SectorId);
+
+    public override object PartTwo() => _data.FirstOrDefault(r => Decrypt(r).Contains("northpole")).SectorId;
+
+    private static string Decrypt((string Encrypted, int SectorId, string Checksum) room) => Decrypt(room.Encrypted, room.SectorId);
+
+    private static string Decrypt(string input, int value)
+    {
+        value %= 26;
+        return new string(input.Select(c => c == '-' ? ' ' : (char)(c + value).Loop(97, 122)).ToArray());
+    }
+
+    private static string GetChecksum(string data)
+        => new(data.Replace("-", "").Distinct().Select(c => (c, data.Count(a => a == c))).OrderByDescending(k => k.Item2).ThenBy(k => k.c).Take(5).Select(k => k.c).ToArray());
+
+    private static (string encrypted, int sectorId, string checksum) Parse(string input)
+    {
+        string[] el = input.ParseExact("{0}[{1}]");
+        return (el[0].Substring(0, el[0].Length - 4), int.Parse(el[0].Substring(el[0].Length - 3)), el[1]);
+    }
+}
--- Tools/IntExtension.cs
+++ Tools/IntExtension.cs
@@ -4,8 +4,8 @@ public static class IntExtension
 {
     public static int Loop(this int value, int min, int max)
     {
-        if (value < min) return max;
-        if (value > max) return min;
+        if (value < min) return max + (value - min + 1) % (max - min + 1);
+        if (value > max) return min + (value - max - 1) % (max - min + 1);
         return value;
     }
 
